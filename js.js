857e42849bbff00f4d1f2109fd2855c3f29f1400
OskarJot <oskar.jarnot@gmail.com>
2023-03-14 15:54:16 +0100

    checkValue validation

--- js.js
+++ js.js
@@ -11,8 +11,17 @@ const checkValue = () => {
 	if (peopleValue.value == '' || peopleValue.value == '0')
 		throw console.log('Nie możesz dzielić rachunku na 0 osób');
 
+	if (
+		chepValue.value !== 10 + '%' &&
+		chepValue.value !== 20 + '%' &&
+		chepValue.value !== 30 + '%'
+	) {
+		throw console.log('Musisz wybrać wysokość napiwku');
+	}
+
 	console.log(priceValue.value);
 	console.log(peopleValue.value);
+	console.log(chepValue.value);
 };
 
 caclBtn.addEventListener('click', checkValue);
