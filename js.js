4926d0f53f757dfe8b1b9c5db06158825dd3c19f
OskarJot <oskar.jarnot@gmail.com>
2023-03-18 09:49:05 +0100

    func calcBill

--- js.js
+++ js.js
@@ -6,22 +6,30 @@ const caclBtn = document.querySelector('.buttonCalc');
 
 const checkValue = () => {
 	if (priceValue.value == '' || priceValue.value == '0')
-		throw console.log('Musisz podać kwote do zapłaty');
+		throw (alertMsg.innerHTML = 'Musisz podać kwote do zapłaty');
 
 	if (peopleValue.value == '' || peopleValue.value == '0')
-		throw console.log('Nie możesz dzielić rachunku na 0 osób');
+		throw (alertMsg.innerHTML = 'Nie możesz dzielić rachunku na 0 osób');
 
 	if (
-		chepValue.value !== 10 + '%' &&
-		chepValue.value !== 20 + '%' &&
-		chepValue.value !== 30 + '%'
+		chepValue.value !== '10' &&
+		chepValue.value !== '20' &&
+		chepValue.value !== '30'
 	) {
-		throw console.log('Musisz wybrać wysokość napiwku');
+		throw (alertMsg.innerHTML = 'Musisz wybrać wysokość napiwku');
 	}
 
-	console.log(priceValue.value);
-	console.log(peopleValue.value);
-	console.log(chepValue.value);
+	caclBill();
+};
+
+const caclBill = () => {
+	const finalBill =
+		(priceValue.value * 1 + priceValue.value * (chepValue.value * 0.01)) /
+		peopleValue.value;
+
+	alertMsg.innerHTML = finalBill + ' zł';
 };
 
 caclBtn.addEventListener('click', checkValue);
+
+// (kwota do zapłaty + (kwota do zapłaty * napiwek)) / ilość osób
